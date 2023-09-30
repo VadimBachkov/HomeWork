bf6e0d4885960d91939db32205c33980688e0db4
VadimBachkov <bachkovvadim@gmail.com>
2023-09-30 16:21:32 +0300

    Practice of array algorithms

--- HomeWork2/src/Main.java
+++ HomeWork2/src/Main.java
@@ -1,17 +1,61 @@
+import java.util.Arrays;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Практика алгоритмов массивов
+        { // Поиск максимального элемента массива
+            int [] array = {0,1,2,3,4,5,5,6,65,8,89,45,4,5};
+            int iMax = 0;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+            for (int i = 0; i < array.length; i++){
+                if(array[i] > array[iMax]){
+                    iMax = i;
+                }
+            }
+            System.out.println("Индекс максимаьного элемент масива - " + iMax + ", значение " + array[iMax]);
+        }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        { // Добавление элемента в массив
+
+            int [] array = {0,2,3};
+            int k = 1;
+
+            for (int i = array.length - 1; i > k; i--){
+                array[i] = array[i-1];
+            }
+            array[k]=1;
+            System.out.println(Arrays.toString(array));
+        }
+
+        { // Удаление элемента массива
+
+            int [] array = {0,2,2,5,74,7,85,4,48,48};
+            int k = 2;
+
+            for (int i = 0; i < array.length - 1; i++){
+                array[i] = array[i + 1];
+            }
+            array[k] = 3;
+            System.out.println(Arrays.toString(array));
+
+
+        }
+
+        { // Сортировка пузырьком
+            int [] array = {0,2,99,5,74,7,85,4,17,21};
+
+            for (int i = 0; i < array.length; i++){
+                for (int j = 0; j < array.length - i -1; j++){
+                    if(array[j] > array[j+1]){
+                        int temp = array[j];
+                        array[j] = array[j+1];
+                        array[j+1] = temp;
+                    }
+                }
+            }
+            System.out.println("Отсортированный массив: " + Arrays.toString(array));
         }
     }
 }
